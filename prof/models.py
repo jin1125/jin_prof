f13b6836b0fbc6552df77771f5bb1794bcf94940
jin <shogo.jinta@gmail.com>
2023-01-07 16:49:19 +0900

    [add]プロフィールのモデルを追加

--- prof/models.py
+++ prof/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Skills(models.Model):
+    skill = models.CharField(max_length=20)
+
+
+class CareersList(models.Model):
+    company = models.CharField(max_length=20)
+    job = models.CharField(max_length=20)
+
+
+class Profile(models.Model):
+    skills = models.ForeignKey(Skills, on_delete=models.CASCADE)
+    careers_list = models.ForeignKey(CareersList, on_delete=models.CASCADE)
+    careers_text = models.TextField()
+    hobbies = models.TextField()
