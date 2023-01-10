156bbdce100689250fddf90a5f0f93ae51455d98
jin <shogo.jinta@gmail.com>
2023-01-10 21:57:14 +0900

    [add]IndexPageTestを追加

--- prof/tests.py
+++ prof/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import resolve
 
-# Create your tests here.
+from prof.views import IndexView
+
+
+class IndexPageTest(TestCase):
+    def test_index_page_returns_200(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_page_uses_expected_view(self):
+        view = resolve('/')
+        self.assertEqual(view.func.view_class, IndexView)
+
+    def test_index_page_uses_expected_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'prof/index.html')
