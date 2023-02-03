68492e3c5ecd9faf7b31fa7cc077dba3be73bce7
jin <shogo.jinta@gmail.com>
2023-02-03 23:58:33 +0900

    [fix]リファクタリング

--- jin_prof/settings.py
+++ jin_prof/settings.py
@@ -161,8 +161,6 @@ MIDDLEWARE = [
 STATIC_URL = 'static/'
 
 if DEBUG:
-    STATICFILES_DIRS = [
-        BASE_DIR / 'static',
-    ]
+    STATICFILES_DIRS = [BASE_DIR / 'static']
 else:
     STATIC_ROOT = BASE_DIR / 'static'
