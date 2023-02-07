83d8accd52cdfd463b2a2800344515aa8916f051
jin <shogo.jinta@gmail.com>
2023-02-07 23:42:33 +0900

    [update]テストコードをリファクタリング

--- tests/tests_error.py
+++ tests/tests_error.py
@@ -9,24 +9,25 @@ class ErrorPagesTest(TestCase):
     def setUp(self):
         """"テストの初期設定"""
         self.factory = RequestFactory()
+        self.test_path = 'test'
 
     def test_403_response(self):
         """ステータス403の場合、403エラーページが表示されるかテスト"""
-        request = self.factory.get('/test/')
+        request = self.factory.get(self.test_path)
         response = render(request, '403.html', status=403)
         self.assertEqual(response.status_code, 403)
         self.assertIn('Forbidden (403)', response.content.decode())
 
     def test_404_response(self):
         """ステータス404の場合、404エラーページが表示されるかテスト"""
-        request = self.factory.get('/test/')
+        request = self.factory.get(self.test_path)
         response = render(request, '404.html', status=404)
         self.assertEqual(response.status_code, 404)
         self.assertIn('Page not found (404)', response.content.decode())
 
     def test_500_response(self):
         """ステータス500の場合、500エラーページが表示されるかテスト"""
-        request = self.factory.get('/test/')
+        request = self.factory.get(self.test_path)
         response = render(request, '500.html', status=500)
         self.assertEqual(response.status_code, 500)
         self.assertIn('Server Error (500)', response.content.decode())
